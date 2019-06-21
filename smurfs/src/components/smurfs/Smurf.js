bd69464690d3d8dd09fbdcfd0c6d9fe64dfd6c07
Awa Melvine <melvineawa@gmail.com>
2019-06-21 10:24:17 +0100

    Style edit and delete buttons

--- smurfs/src/components/smurfs/Smurf.js
+++ smurfs/src/components/smurfs/Smurf.js
@@ -6,6 +6,8 @@ const Smurf = ({ smurf }) => {
       <h3>{smurf.name}</h3>
       <p>{`${smurf.age} years old`}</p>
       <p>{`${smurf.height} tall`}</p>
+      <span className="delete-smurf">x</span>
+      <span className="edit-smurf">edit</span>
     </div>
   );
 };
