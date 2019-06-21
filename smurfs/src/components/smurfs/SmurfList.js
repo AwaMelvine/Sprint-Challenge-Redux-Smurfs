ce1b1a3b916930d391b70ef000c07e28b1383430
Awa Melvine <melvineawa@gmail.com>
2019-06-21 09:21:35 +0100

    Create smurf list component

---
+++ smurfs/src/components/smurfs/SmurfList.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { fetchSmurfs } from "../../actions";
+
+class SmurfList extends Component {
+  componentDidMount() {
+    this.props.fetchSmurfs();
+  }
+  render() {
+    const { smurfs } = this.props;
+    return (
+      <div>
+        {smurfs.map(smurf => (
+          <Smurf key={smurf.id} smurf={smurf} />
+        ))}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  smurfs: state.smurfs
+});
+
+export default (mapStateToProps, { fetchSmurfs })(SmurfList);
